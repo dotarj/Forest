b294a78cf13ab27446c26d1b868a246ee6365bd5
Arjen Post <mail@arjenpost.nl>
2016-12-11 19:38:27 +0100

    added empty double array trie class

---
+++ src/Forest/DoubleArrayTrie.cs
@@ -0,0 +1,19 @@
+// Copyright (c) Arjen Post. See License.txt in the project root for license information.
+
+using System;
+
+namespace Forest
+{
+    public class DoubleArrayTrie
+    {
+        public void Add(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
