175bc04e1dbd29aec874670925cf7954e00be898
Arjen Post <mail@arjenpost.nl>
2016-12-16 07:00:34 +0100

    unit tests using nested classes does not work with VSC

--- test/Forest.Test/DoubleArrayTrieTests.cs
+++ test/Forest.Test/DoubleArrayTrieTests.cs
@@ -3,116 +3,113 @@ using Xunit;
 
 namespace Forest.Test
 {
-    public abstract class DoubleArrayTrieTests
+    public class DoubleArrayTrieTests_TheAddMethod
     {
-        public class TheAddMethod : DoubleArrayTrieTests
+        [Fact]
+        public void ShouldThrowIfKeyIsNull()
         {
-            [Fact]
-            public void ShouldThrowIfKeyIsNull()
-            {
-                // Arrange
-                string key = null;
-                var trie = new DoubleArrayTrie();
+            // Arrange
+            string key = null;
+            var trie = new DoubleArrayTrie();
 
-                // Assert
-                Assert.Throws<ArgumentNullException>(() => trie.Add(key));
-            }
-
-            [Fact]
-            public void ShouldAddKey()
-            {
-                // Arrange
-                var key = "bachelor";
-                var trie = new DoubleArrayTrie();
-
-                // Act
-                trie.Add(key);
-
-                // Assert
-                trie.ContainsKey(key);
-            }
-
-            [Fact]
-            public void ShouldAddAKeyWithoutCollisions()
-            {
-                // Arrange
-                var key = "jar";
-                var trie = new DoubleArrayTrie();
-                
-                trie.Add("bachelor");
-
-                // Act
-                trie.Add(key);
-
-                // Assert
-                trie.ContainsKey(key);
-            }
-
-            [Fact]
-            public void ShouldAddAKeyWithCollisions1()
-            {
-                // Arrange
-                var key = "badge";
-                var trie = new DoubleArrayTrie();
-                
-                trie.Add("bachelor");
-                trie.Add("jar");
-
-                // Act
-                trie.Add(key);
-
-                // Assert
-                trie.ContainsKey(key);
-            }
-
-            [Fact]
-            public void ShouldAddAKeyWithCollisions2()
-            {
-                // Arrange
-                var key = "baby";
-                var trie = new DoubleArrayTrie();
-                
-                trie.Add("bachelor");
-                trie.Add("jar");
-                trie.Add("badge");
-
-                // Act
-                trie.Add(key);
-
-                // Assert
-                trie.ContainsKey(key);
-            }
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => trie.Add(key));
         }
 
-        public class TheContainsKeyMethod : DoubleArrayTrieTests
+        [Fact]
+        public void ShouldAddKey()
         {
-            [Fact]
-            public void ShouldThrowIfKeyIsNull()
-            {
-                // Arrange
-                string key = null;
-                var trie = new DoubleArrayTrie();
+            // Arrange
+            var key = "bachelor";
+            var trie = new DoubleArrayTrie();
 
-                // Assert
-                Assert.Throws<ArgumentNullException>(() => trie.ContainsKey(key));
-            }
+            // Act
+            trie.Add(key);
 
-            [Fact]
-            public void ShouldReturnTrueIfKeyIsFound()
-            {
-                // Arrange
-                var key = "bachelor";
-                var trie = new DoubleArrayTrie();
+            // Assert
+            trie.ContainsKey(key);
+        }
 
-                var @base = new [] { 0, 4, 0, 1, -15, -1, -12, 1, 0, 0, 0, 0, 0, 0, 0, -9 };
-                var check = new [] { 0, 0, 0, 7,   3,  3,   3, 1, 0, 0, 0, 0, 0, 0, 0,  1 };
-                var tail = "0helor#??ar#ge#y#".ToCharArray();
+        [Fact]
+        public void ShouldAddAKeyWithoutCollisions()
+        {
+            // Arrange
+            var key = "jar";
+            var trie = new DoubleArrayTrie();
 
-                trie.SetArrays(@base, check, tail);
+            trie.Add("bachelor");
 
-                // Assert
-                Assert.True(trie.ContainsKey(key));
-            }
+            // Act
+            trie.Add(key);
+
+            // Assert
+            trie.ContainsKey(key);
+        }
+
+        [Fact]
+        public void ShouldAddAKeyWithCollisions1()
+        {
+            // Arrange
+            var key = "badge";
+            var trie = new DoubleArrayTrie();
+
+            trie.Add("bachelor");
+            trie.Add("jar");
+
+            // Act
+            trie.Add(key);
+
+            // Assert
+            trie.ContainsKey(key);
+        }
+
+        [Fact]
+        public void ShouldAddAKeyWithCollisions2()
+        {
+            // Arrange
+            var key = "baby";
+            var trie = new DoubleArrayTrie();
+
+            trie.Add("bachelor");
+            trie.Add("jar");
+            trie.Add("badge");
+
+            // Act
+            trie.Add(key);
+
+            // Assert
+            trie.ContainsKey(key);
+        }
+    }
+
+    public class DoubleArrayTests_TheContainsKeyMethod
+    {
+        [Fact]
+        public void ShouldThrowIfKeyIsNull()
+        {
+            // Arrange
+            string key = null;
+            var trie = new DoubleArrayTrie();
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => trie.ContainsKey(key));
+        }
+
+        [Fact]
+        public void ShouldReturnTrueIfKeyIsFound()
+        {
+            // Arrange
+            var key = "bachelor";
+            var trie = new DoubleArrayTrie();
+
+            var @base = new [] { 0, 4, 0, 1, -15, -1, -12, 1, 0, 0, 0, 0, 0, 0, 0, -9 };
+            var check = new [] { 0, 0, 0, 7,   3,  3,   3, 1, 0, 0, 0, 0, 0, 0, 0,  1 };
+            var tail = "0helor#??ar#ge#y#".ToCharArray();
+
+            trie.SetArrays(@base, check, tail);
+
+            // Assert
+            Assert.True(trie.ContainsKey(key));
         }
     }
 }
